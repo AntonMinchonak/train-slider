8f49414ebd9e7116b62c498b16dae76af0f92f35
Anton Minchonak <anton.minchonak@gmail.com>
2021-12-27 22:31:19 +0300

    done without disabled click

--- script.js
+++ script.js
@@ -4,51 +4,86 @@ let buttonLeft = buttons[0];
 let buttonRight = buttons[1];
 let navigationWraps = document.querySelectorAll(".navigation-wrap");
 let navigations = document.querySelectorAll(".navigation-item");
-
+let mainWidth = parseFloat(window.getComputedStyle(main).width);
+let items = document.querySelectorAll(".item");
 let slideCount = 1;
-let leftPos;
+let animationEnd = true;
+let pressFlag = false;
+let pressPoint = 0;
+let upPoint = parseFloat(window.getComputedStyle(main).left);
+let startLeftPos =0
 
-console.log(window.getComputedStyle(main).left);
+function switchSlide(index, isAnime = true) {
+  if (animationEnd) {
+    animationEnd = false;
+    if (isAnime) {
+      main.style.transition = "0.3s ease-in-out";
+    } else {
+      main.style.transition = "none";
+    }
+    slideCount = index;
+    main.style.left = -mainWidth * slideCount + "px";
+    main.ontransitionend = () => {
+      main.style.transition = "none";//???????????????????????
+      animationEnd = true;
+      if (slideCount === 0) switchSlide(items.length - 2, false);
+      if (slideCount === 7) switchSlide(1, false);
+      navigationWraps[0].parentElement.querySelector(".navigation-item--active").classList.remove("navigation-item--active");
+      navigations[slideCount - 1].classList.add("navigation-item--active");
+      animationEnd = true;
+      upPoint = parseFloat(window.getComputedStyle(main).left);
+    };
+  }
+
+}
+
+function arfterSwitch() {
+  if (Math.abs(event.pageX - pressPoint) > 100) {
+    event.pageX - pressPoint < 0 ? switchSlide(slideCount + 1) : switchSlide(slideCount - 1);
+  } else {
+    switchSlide(slideCount);
+  }
+
+}
 
 buttonLeft.onclick = () => {
-  if (slideCount === 1) {
-    slideCount = 7;
-    main.style.transition = "none";
-    main.style.left = -3914 + "px";
-    navigations[0].classList.remove("navigation-item--active");
-  }
-  setTimeout(() => {
-    main.style.transition = "0.3s ease-in-out";
-    slideCount--;
-    main.style.left = -559.376 * slideCount + "px";
-    navigations[slideCount - 1].classList.add("navigation-item--active");
-    navigations[slideCount].classList.remove("navigation-item--active");
-  }, 0);
+  switchSlide(slideCount - 1);
 };
 
 buttonRight.onclick = () => {
-  if (slideCount === 6) {
-    slideCount = 0;
-    main.style.transition = "none";
-    main.style.left = 0 + "px";
-    navigations[5].classList.remove("navigation-item--active");
-  }
-  setTimeout(() => {
-    main.style.transition = "0.3s ease-in-out";
-    slideCount++;
-    main.style.left = -559.376 * slideCount + "px";
-    navigations[slideCount - 1].classList.add("navigation-item--active");
-    navigations[slideCount - 2].classList.remove("navigation-item--active");
-  }, 0);
+  switchSlide(slideCount + 1);
 };
-
 
 for (let i = 0; i < navigations.length; i++) {
   navigationWraps[i].onclick = () => {
-    navigations[slideCount - 1].classList.remove("navigation-item--active");
-    main.style.transition = "0.3s ease-in-out";
-    main.style.left = -559.376 * (i + 1) + "px";
-    navigations[i].classList.add("navigation-item--active");
-    slideCount = i + 1;
+  switchSlide(i+1);
   };
 }
+
+main.onmousedown = (event) => {
+  if (animationEnd) {
+    pressFlag = true;
+    pressPoint = event.pageX;
+  }
+};
+
+main.onmousemove = (event) => {
+  if (pressFlag) {
+    main.style.left = event.pageX - pressPoint + upPoint + "px";
+  }
+};
+
+main.onmouseup = () => {
+  if (pressFlag) {
+    pressFlag = false;
+    arfterSwitch();
+   
+  }
+}
+
+main.onmouseout = () => {
+  if (pressFlag) {
+    pressFlag = false;
+    arfterSwitch(); 
+  }
+};
